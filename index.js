9eb0dc83b37841c96ac1cd5c8c421fdd0c394c8f
Luis Gomez <luis.gomez@bigcommerce.com>
2022-07-04 13:55:24 -0500

    test

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const { ApolloServer} = require('apollo-server');
 const typeDefs = require('./db/schema.graphql');
 const resolvers = require('./db/resolvers');
 const conectarDB = require('./config/db');
-
+const cors = require('cors');
 //Connect to DB 
 conectarDB();
+
 
 
 //server
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    cors: {
+        origin: ["https://colorsapp-luisdonaldogomez.netlify.app/"]
+    },
+    
 });
 
 //Run server
